a1f18115e9e9cd24735d8fdd61ce54a14e33b80c
u223699 <simon.erhardt@sbb.ch>
2017-09-04 10:40:21 +0200

    create state directory if it not exists

--- root/usr/local/bin/docker-run.sh
+++ root/usr/local/bin/docker-run.sh
@@ -3,6 +3,11 @@ set -e
 
 echo "Starting environment..."
 confd -onetime -backend env
+
+if [ ! -d /data/state ]; then
+  mkdir /data/state
+fi
+
 chown -R zpush:zpush /data /opt/zpush
 
 # run application
